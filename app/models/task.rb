791d1784a7fa73e054e9530c18a19d006389bc0f
Yap Zhi Heng <yapzhhg@gmail.com>
2022-01-24 22:22:15 +0800

    Fix task delete bug

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
     has_and_belongs_to_many :tags, :join_table => :tag_tasks
     # Ensure cascading delete
     before_destroy do
+        tag_tasks = TagTask.where(task_id: self.id)
         tag_tasks.each { |tag_task| tag_task.destroy }
     end
 end
